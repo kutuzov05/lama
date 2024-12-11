b41d4e81fdc49b63314f652832ca7c071114ad83
kutuzov05 <144845962+kutuzov05@users.noreply.github.com>
2024-12-11 17:06:44 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import pygame
 
 
 pygame.init()
-width, height = 500, 500
+width, height = 750, 750
 screen = pygame.display.set_mode((width, height))
 FPS = 60
 clock = pygame.time.Clock()
@@ -10,3 +10,8 @@ clock = pygame.time.Clock()
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BLUE = (215, 235, 255)
+RED = (255, 215, 215)
+
+words = ["Wasser", "Feuer"]
+slot_l, slot_r = "Wasser", "Feuer"
